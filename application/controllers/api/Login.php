1d0c5b2d9e41e78066fe63bd2ac20ac438765ad8
Satrio Adhi Purbo <satrioadhi04@gmail.com>
2020-03-12 21:43:09 +0700

    up

--- application/controllers/api/Login.php
+++ application/controllers/api/Login.php
@@ -19,29 +19,36 @@ class Login extends REST_Controller
 		
 		//$pegawai = $this->pegawai->login($username,$password);
 		$cek_owner=$this->pegawai->auth_owner($username,$password);
-		if($cek_owner->num_rows() > 0){ //jika login sebagai dosen
-			$data=$cek_owner->row_array();
-			$this->response([
-				'status' => TRUE,
-				'message' => 'login owner sukses!',
-                'data' => $data
-			], REST_Controller::HTTP_OK); 
-		} else{
-			$cek_cs=$this->pegawai->auth_cs($username,$password);
-			if($cek_cs->num_rows() > 0){
-				$data=$cek_cs->row_array();
-				$this->response([
-					'status' => TRUE,
-					'message' => 'login cs sukses!',
-					'data' => $data
-				], REST_Controller::HTTP_OK);
-			}else{
-				$this->response([
-					'status' => false,
-					'message' => 'id tidak ditemukan!'
-				], REST_Controller::HTTP_NOT_FOUND); 
-			}
-		}
+		$data=$cek_owner->row_array();
+		$this->response([
+			'status' => TRUE,
+			'message' => 'login owner sukses!',
+			'data' => $data
+		], REST_Controller::HTTP_OK); 
+		
+		// if($cek_owner->num_rows() > 0){ //jika login sebagai dosen
+		// 	$data=$cek_owner->row_array();
+		// 	$this->response([
+		// 		'status' => TRUE,
+		// 		'message' => 'login owner sukses!',
+        //         'data' => $data
+		// 	], REST_Controller::HTTP_OK); 
+		// } else{
+		// 	$cek_cs=$this->pegawai->auth_cs($username,$password);
+		// 	if($cek_cs->num_rows() > 0){
+		// 		$data=$cek_cs->row_array();
+		// 		$this->response([
+		// 			'status' => TRUE,
+		// 			'message' => 'login cs sukses!',
+		// 			'data' => $data
+		// 		], REST_Controller::HTTP_OK);
+		// 	}else{
+		// 		$this->response([
+		// 			'status' => false,
+		// 			'message' => 'id tidak ditemukan!'
+		// 		], REST_Controller::HTTP_NOT_FOUND); 
+		// 	}
+		// }
     }
 
 }
