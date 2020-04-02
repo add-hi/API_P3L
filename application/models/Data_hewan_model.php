3c8d5312c2e296f7c8f6a3efe94cda8fa5aa118b
Satrio Adhi Purbo <satrioadhi04@gmail.com>
2020-04-02 16:00:18 +0700

    update response data hewan dan member

--- application/models/Data_hewan_model.php
+++ application/models/Data_hewan_model.php
@@ -5,7 +5,7 @@ class Data_hewan_model extends CI_Model{
     //untuk tampil semua data
     public function getLog($id = null){
         if($id === null){
-            return $this->db->get('data_hewan')->result_array();
+            return $this->db->query("SELECT data_hewan.id_hewan, data_hewan.id_jenis, data_hewan.id_ukuran, data_hewan.nama, data_hewan.tgl_lhr, data_hewan.created_at, data_hewan.update_at, data_hewan.delete_at, data_hewan.id_pegawai_cs, data_hewan.id_pegawai_kasir, data_hewan.id_member,jenis_hewan.jenis , ukuran_hewan.ukuran , member.nama AS 'nama_member' FROM data_hewan JOIN jenis_hewan USING (id_jenis) JOIN ukuran_hewan USING (id_ukuran) JOIN member USING (id_member)")->result_array();
         } else{
             return $this->db->get_where('data_hewan', ['id_hewan' => $id]) ->result_array();
         }
--- application/models/Member_model.php
+++ application/models/Member_model.php
@@ -5,7 +5,7 @@ class Member_model extends CI_Model{
     //untuk tampil semua data
     public function getLog($id = null){
         if($id === null){
-            return $this->db->get('member')->result_array();
+            return $this->db->query("SELECT M.id_member, M.id_pegawai_cs, M.id_pegawai_kasir, M.nama, M.no_telp, M.tgl_lhr, M.alamat, M.created_at, M.update_at, M.delete_at, M.status , P1.nama AS nama_cs , P2.nama AS nama_kasir FROM member M JOIN pegawai P1 ON (M.id_pegawai_cs = P1.id_pegawai) JOIN pegawai P2 ON (M.id_pegawai_kasir= P2.id_pegawai)")->result_array();
         } else{
             return $this->db->get_where('member', ['id_member' => $id]) ->result_array();
         }
@@ -15,7 +15,7 @@ class Member_model extends CI_Model{
     //tidak menampilkan yang ter soft delete 
     public function getMember($id_member = null){
         if($id_member === null){
-            return $this->db->get_where('member', ['delete_at' => null])->result_array();
+            return $this->db->query("SELECT M.id_member, M.id_pegawai_cs, M.id_pegawai_kasir, M.nama, M.no_telp, M.tgl_lhr, M.alamat, M.created_at, M.update_at, M.delete_at, M.status , P1.nama AS nama_cs , P2.nama AS nama_kasir FROM member M JOIN pegawai P1 ON (M.id_pegawai_cs = P1.id_pegawai) JOIN pegawai P2 ON (M.id_pegawai_kasir= P2.id_pegawai) WHERE M.delete_at IS NULL")->result_array();
         } else{
             return $this->db->get_where('member', ['id_member' => $id_member]) ->result_array();
         }
