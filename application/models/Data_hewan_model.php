1d65b7ad4e69e6e8d2c879710db5e5d1b69d50ee
Satrio Adhi Purbo <satrioadhi04@gmail.com>
2020-04-02 16:07:36 +0700

    UPDATE RESPONSE

--- application/models/Data_hewan_model.php
+++ application/models/Data_hewan_model.php
@@ -5,7 +5,7 @@ class Data_hewan_model extends CI_Model{
     //untuk tampil semua data
     public function getLog($id = null){
         if($id === null){
-            return $this->db->query("SELECT data_hewan.id_hewan, data_hewan.id_jenis, data_hewan.id_ukuran, data_hewan.nama, data_hewan.tgl_lhr, data_hewan.created_at, data_hewan.update_at, data_hewan.delete_at, data_hewan.id_pegawai_cs, data_hewan.id_pegawai_kasir, data_hewan.id_member,jenis_hewan.jenis , ukuran_hewan.ukuran , member.nama AS 'nama_member' FROM data_hewan JOIN jenis_hewan USING (id_jenis) JOIN ukuran_hewan USING (id_ukuran) JOIN member USING (id_member)")->result_array();
+            return $this->db->query("SELECT DH.id_hewan, DH.id_jenis, DH.id_ukuran, DH.nama, DH.tgl_lhr, DH.created_at, DH.update_at, DH.delete_at, DH.id_pegawai_cs, DH.id_pegawai_kasir, DH.id_member, J.jenis , U.ukuran , M.nama AS nama_member , P1.nama AS nama_cs , P2.nama AS nama_kasir FROM data_hewan DH JOIN jenis_hewan J ON (DH.id_jenis = J.id_jenis) JOIN ukuran_hewan U ON (DH.id_jenis = U.id_ukuran) JOIN member M ON (DH.id_member = M.id_member) JOIN pegawai P1 ON (DH.id_pegawai_cs = P1.id_pegawai) JOIN pegawai P2 ON (DH.id_pegawai_kasir = P2.id_pegawai)")->result_array();
         } else{
             return $this->db->get_where('data_hewan', ['id_hewan' => $id]) ->result_array();
         }
@@ -15,7 +15,7 @@ class Data_hewan_model extends CI_Model{
     //tidak menampilkan yang ter soft delete  
     public function getData_hewan($id_hewan = null){
         if($id_hewan === null){
-            return $this->db->query("SELECT data_hewan.id_hewan, data_hewan.id_jenis, data_hewan.id_ukuran, data_hewan.nama, data_hewan.tgl_lhr, data_hewan.created_at, data_hewan.update_at, data_hewan.delete_at, data_hewan.id_pegawai_cs, data_hewan.id_pegawai_kasir, data_hewan.id_member,jenis_hewan.jenis , ukuran_hewan.ukuran , member.nama AS 'nama_member' FROM data_hewan JOIN jenis_hewan USING (id_jenis) JOIN ukuran_hewan USING (id_ukuran) JOIN member USING (id_member) WHERE data_hewan.delete_at IS NULL")->result_array();
+            return $this->db->query("SELECT DH.id_hewan, DH.id_jenis, DH.id_ukuran, DH.nama, DH.tgl_lhr, DH.created_at, DH.update_at, DH.delete_at, DH.id_pegawai_cs, DH.id_pegawai_kasir, DH.id_member, J.jenis , U.ukuran , M.nama AS nama_member , P1.nama AS nama_cs , P2.nama AS nama_kasir FROM data_hewan DH JOIN jenis_hewan J ON (DH.id_jenis = J.id_jenis) JOIN ukuran_hewan U ON (DH.id_jenis = U.id_ukuran) JOIN member M ON (DH.id_member = M.id_member) JOIN pegawai P1 ON (DH.id_pegawai_cs = P1.id_pegawai) JOIN pegawai P2 ON (DH.id_pegawai_kasir = P2.id_pegawai) WHERE DH.delete_at IS NULL")->result_array();
         } else{
             return $this->db->get_where('data_hewan', ['id_hewan' => $id_hewan]) ->result_array();
         }
