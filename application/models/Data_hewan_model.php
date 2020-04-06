bdf5b61af19b080c21b49256ec3ca9c6b3545832
Satrio Adhi Purbo <satrioadhi04@gmail.com>
2020-04-06 14:12:03 +0700

    fixed bug ukuran hewan

--- application/models/Data_hewan_model.php
+++ application/models/Data_hewan_model.php
@@ -6,7 +6,7 @@ class Data_hewan_model extends CI_Model{
     public function getLog($id = null){
         if($id === null){
             return $this->db->query("SELECT DH.id_hewan, DH.id_jenis, DH.id_ukuran, DH.nama, DH.tgl_lhr, DH.created_at, DH.update_at, DH.delete_at, DH.id_pegawai_cs, DH.id_pegawai_kasir, DH.id_member, DH.created_by, DH.deleted_by ,J.jenis , U.ukuran , M.nama AS nama_member , P1.nama AS nama_cs , P2.nama AS nama_kasir , P3.nama AS nama_created_by , P4.nama AS nama_deleted_by
-            FROM data_hewan DH JOIN jenis_hewan J ON (DH.id_jenis = J.id_jenis) JOIN ukuran_hewan U ON (DH.id_jenis = U.id_ukuran) JOIN member M ON (DH.id_member = M.id_member) JOIN pegawai P1 ON (DH.id_pegawai_cs = P1.id_pegawai) JOIN pegawai P2 ON (DH.id_pegawai_kasir = P2.id_pegawai) JOIN pegawai P3 ON (DH.created_by = P3.id_pegawai) JOIN pegawai P4 ON (DH.deleted_by = P4.id_pegawai)")->result_array();
+            FROM data_hewan DH JOIN jenis_hewan J ON (DH.id_jenis = J.id_jenis) JOIN ukuran_hewan U ON (DH.id_ukuran = U.id_ukuran) JOIN member M ON (DH.id_member = M.id_member) JOIN pegawai P1 ON (DH.id_pegawai_cs = P1.id_pegawai) JOIN pegawai P2 ON (DH.id_pegawai_kasir = P2.id_pegawai) JOIN pegawai P3 ON (DH.created_by = P3.id_pegawai) JOIN pegawai P4 ON (DH.deleted_by = P4.id_pegawai)")->result_array();
         } else{
             return $this->db->get_where('data_hewan', ['id_hewan' => $id]) ->result_array();
         }
@@ -17,7 +17,7 @@ class Data_hewan_model extends CI_Model{
     public function getData_hewan($id_hewan = null){
         if($id_hewan === null){
             return $this->db->query("SELECT DH.id_hewan, DH.id_jenis, DH.id_ukuran, DH.nama, DH.tgl_lhr, DH.created_at, DH.update_at, DH.delete_at, DH.id_pegawai_cs, DH.id_pegawai_kasir, DH.id_member, DH.created_by, DH.deleted_by ,J.jenis , U.ukuran , M.nama AS nama_member , P1.nama AS nama_cs , P2.nama AS nama_kasir , P3.nama AS nama_created_by , P4.nama AS nama_deleted_by
-            FROM data_hewan DH JOIN jenis_hewan J ON (DH.id_jenis = J.id_jenis) JOIN ukuran_hewan U ON (DH.id_jenis = U.id_ukuran) JOIN member M ON (DH.id_member = M.id_member) JOIN pegawai P1 ON (DH.id_pegawai_cs = P1.id_pegawai) JOIN pegawai P2 ON (DH.id_pegawai_kasir = P2.id_pegawai) JOIN pegawai P3 ON (DH.created_by = P3.id_pegawai) JOIN pegawai P4 ON (DH.deleted_by = P4.id_pegawai) WHERE DH.delete_at IS NULL")->result_array();
+            FROM data_hewan DH JOIN jenis_hewan J ON (DH.id_jenis = J.id_jenis) JOIN ukuran_hewan U ON (DH.id_ukuran = U.id_ukuran) JOIN member M ON (DH.id_member = M.id_member) JOIN pegawai P1 ON (DH.id_pegawai_cs = P1.id_pegawai) JOIN pegawai P2 ON (DH.id_pegawai_kasir = P2.id_pegawai) JOIN pegawai P3 ON (DH.created_by = P3.id_pegawai) JOIN pegawai P4 ON (DH.deleted_by = P4.id_pegawai) WHERE DH.delete_at IS NULL")->result_array();
         } else{
             return $this->db->get_where('data_hewan', ['id_hewan' => $id_hewan]) ->result_array();
         }
